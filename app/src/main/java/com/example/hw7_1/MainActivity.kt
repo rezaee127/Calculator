93d0dbc967ea20ca6bdf9eae5734c91e641a186a
razie rezaee <rezaie127@yahoo.com>
2022-02-23 10:17:23 +0330

    add toast

--- app/src/main/java/com/example/hw7_1/MainActivity.kt
+++ app/src/main/java/com/example/hw7_1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.hw7_1
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.hw7_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,18 +23,18 @@ class MainActivity : AppCompatActivity() {
 
         onClick()
 
-        delete()
 
+        binding.delete.setOnClickListener {
+            delete()
+        }
 
     }
 
     private fun delete() {
-    binding.delete.setOnClickListener{
         a = 0F
         b = 0F
         binding.textView.text = "0"
         binding.text.text = "0"
-    }
 }
 
     @SuppressLint("SetTextI18n")
@@ -41,86 +42,107 @@ class MainActivity : AppCompatActivity() {
         binding.b0.setOnClickListener {
             binding.textView.text = "" + binding.textView.text + binding.b0.text
             binding.text.text = "" + binding.text.text + binding.b0.text
+            Toast.makeText(this,"${binding.b0.text}",Toast.LENGTH_SHORT).show()
+
         }
         binding.b1.setOnClickListener {
             binding.textView.text = "" + binding.textView.text + binding.b1.text
             binding.text.text = "" + binding.text.text + binding.b1.text
+            Toast.makeText(this,"${binding.b1.text}",Toast.LENGTH_SHORT).show()
         }
         binding.b2.setOnClickListener {
             binding.textView.text = "" + binding.textView.text + binding.b2.text
             binding.text.text = "" + binding.text.text + binding.b2.text
+            Toast.makeText(this,"${binding.b2.text}",Toast.LENGTH_SHORT).show()
         }
         binding.b3.setOnClickListener {
             binding.textView.text = "" + binding.textView.text + binding.b3.text
             binding.text.text = "" + binding.text.text + binding.b3.text
+            Toast.makeText(this,"${binding.b3.text}",Toast.LENGTH_SHORT).show()
         }
         binding.b4.setOnClickListener {
             binding.textView.text = "" + binding.textView.text + binding.b4.text
             binding.text.text = "" + binding.text.text + binding.b4.text
+            Toast.makeText(this,"${binding.b4.text}",Toast.LENGTH_SHORT).show()
         }
         binding.b5.setOnClickListener {
             binding.textView.text = "" + binding.textView.text + binding.b5.text
             binding.text.text = "" + binding.text.text + binding.b5.text
+            Toast.makeText(this,"${binding.b5.text}",Toast.LENGTH_SHORT).show()
         }
         binding.b6.setOnClickListener {
             binding.textView.text = "" + binding.textView.text + binding.b6.text
             binding.text.text = "" + binding.text.text + binding.b6.text
+            Toast.makeText(this,"${binding.b6.text}",Toast.LENGTH_SHORT).show()
         }
         binding.b7.setOnClickListener {
             binding.textView.text = "" + binding.textView.text + binding.b7.text
             binding.text.text = "" + binding.text.text + binding.b7.text
+            Toast.makeText(this,"${binding.b7.text}",Toast.LENGTH_SHORT).show()
         }
         binding.b8.setOnClickListener {
             binding.textView.text = "" + binding.textView.text + binding.b8.text
             binding.text.text = "" + binding.text.text + binding.b8.text
+            Toast.makeText(this,"${binding.b8.text}",Toast.LENGTH_SHORT).show()
         }
         binding.b9.setOnClickListener {
             binding.textView.text = "" + binding.textView.text + binding.b9.text
             binding.text.text = "" + binding.text.text + binding.b9.text
+            Toast.makeText(this,"${binding.b9.text}",Toast.LENGTH_SHORT).show()
         }
         binding.bn.setOnClickListener {
             binding.textView.text = "" + binding.textView.text + binding.bn.text
             binding.text.text = "" + binding.text.text + binding.bn.text
+            Toast.makeText(this,"${binding.bn.text}",Toast.LENGTH_SHORT).show()
         }
 
         binding.ba.setOnClickListener {
+            Toast.makeText(this,"${binding.ba.text}",Toast.LENGTH_SHORT).show()
             if ( binding.text.text!="") {
                 flag = true
                 a = (binding.text.text.toString()).toFloat()
                 binding.text.text = ""
                 binding.textView.text = "$a+"
                 operator = "+"
-            }
+            }else
+                delete()
         }
         binding.bd.setOnClickListener {
+            Toast.makeText(this,"${binding.bd.text}",Toast.LENGTH_SHORT).show()
             if ( binding.text.text!="") {
                 flag = true
                 a = (binding.text.text.toString()).toFloat()
                 binding.text.text = ""
                 binding.textView.text = "$a/"
                 operator = "/"
-            }
+            }else
+                delete()
         }
         binding.bmul.setOnClickListener {
+            Toast.makeText(this,"${binding.bmul.text}",Toast.LENGTH_SHORT).show()
             if ( binding.text.text!="") {
                 flag = true
                 a = (binding.text.text.toString()).toFloat()
                 binding.text.text = ""
                 binding.textView.text = "$a*"
                 operator = "*"
-            }
+            }else
+                delete()
         }
         binding.bmin.setOnClickListener {
+            Toast.makeText(this,"${binding.bmin.text}",Toast.LENGTH_SHORT).show()
             if ( binding.text.text!="") {
                 flag = true
                 a = (binding.text.text.toString()).toFloat()
                 binding.text.text = ""
                 binding.textView.text = "$a-"
                 operator = "-"
-            }
+            }else
+                delete()
         }
 
         binding.be.setOnClickListener {
+            Toast.makeText(this,"${binding.be.text}",Toast.LENGTH_SHORT).show()
             if (flag && binding.text.text!="") {
                 b = binding.text.text.toString().toFloat()
                 if (operator == "+") {
@@ -144,13 +166,14 @@ class MainActivity : AppCompatActivity() {
                         binding.textView.text =
                             "" + binding.textView.text + binding.be.text + binding.text.text
                     } else {
-                        binding.text.text = "divide to 0"
-                        binding.textView.text = "divide to 0"
+                        Toast.makeText(this,"Divide by zero",Toast.LENGTH_SHORT).show()
+                        delete()
                     }
 
                 }
 
-            }
+            }else
+                delete()
             flag=false
         }
     }
